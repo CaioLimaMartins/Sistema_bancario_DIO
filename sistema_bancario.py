753a4d9c6655ff70c4b1a4d83645187c448a8ae2
Caio Lima Martins <161627687+CaioLimaMartins@users.noreply.github.com>
2025-09-02 01:10:52 +0000

    Ajustes para depositos negativos

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -17,6 +17,7 @@ numero_deposito = 1
 while True:
     opção = input(menu)
     
+    
     if opção == '1':
         valor_saque = float(input('Digite o valor do saque: '))
         if contador_saque <= 3:
@@ -26,6 +27,8 @@ while True:
                 print(f'Seu saldo atual é de R${saldo: .2f}')
                 print(f'Você ainda possui {3 - contador_saque} saques.')
                 contador_saque += 1
+                extrato += f'Saque número {numero_saque} no valor de {valor_saque: .2f}| Saldo: {saldo: .2f} \n'
+                numero_saque += 1
             else:
                 if valor_saque > saldo: 
                     print(f'Seu saldo atual de R${saldo: .2f} é insuficiente para sacar a quantia desejada.')
@@ -33,24 +36,27 @@ while True:
                     print('O valor máximo de saque é de R$ 500.00')
         else:
             print('O limite de saques foi atingindo')
-
-        extrato += f'Saque número {numero_saque} no valor de {valor_saque: .2f}| Saldo: {saldo: .2f} \n'
-        numero_saque += 1
+            
         
     elif opção == '2':
         valor_deposito = float(input('Digite o valor do deposito: '))
+        if valor_deposito >= 0:
+            saldo += valor_deposito
+            print(f'Seu saldo atual é de R${saldo: .2f}')
+            extrato += f'Deposito número {numero_deposito} no valor de{valor_deposito: .2f} | Saldo:{saldo: .2f} \n'  
+            numero_deposito += 1
         
-        saldo += valor_deposito
-        print(f'Seu saldo atual é de R${saldo: .2f}')
+        else:
+            print('Só é possível depositar valores positvos.')
         
-        extrato += f'Deposito número {numero_deposito} no valor de{valor_deposito: .2f} | Saldo:{saldo: .2f} \n'  
-        numero_deposito += 1
-        
+    
     elif opção == '3':
           print(extrato)
           
+    
     elif opção == '4':
         break
+    
     
     else:
         print('Informe uma opção válida!')
